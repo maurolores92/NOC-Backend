12d73493d6bc16bacb79aa2402b0633316717c68
Glitch (glitch-hello-node) <none>
2024-05-30 22:58:35 +0000

    💗🎗 Checkpoint ./server.js:68197850/13556 ./src/upload.js:68197850/6510

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const ping = require("ping");
 const cors = require("cors");
-const uploadRoute = require("./src/upload");
+const { Client } = require('ssh2');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/upload", uploadRoute);
 
 // Usa el puerto que Glitch proporciona, o 3000 si no se proporciona ninguno
 const port = process.env.PORT || 3000;
@@ -29,9 +28,23 @@ app.get("/ping/:ip", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   // Registra la URL de tu servidor en Glitch
   console.log(
     `Server listening at https://chipped-sophisticated-grey.glitch.me`
   );
+
+  // Conexión a la antena Ubiquiti
+  const conn = new Client();
+  conn.on('ready', () => {
+    console.log('Connected successfully to Ubiquiti antenna');
+    conn.end();
+  }).on('error', (error) => {
+    console.error("Failed to connect to Ubiquiti antenna:", error.message);
+  }).connect({
+    host: "186.1.242.50",
+    port: 8889,
+    username: "nortech",
+    password: "Nor3164!",
+  });
 });
