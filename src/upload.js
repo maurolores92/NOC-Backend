3d3b7a5b8ee1e00c7096d0733c77d0cf14af90ac
Glitch (glitch-hello-node) <none>
2024-05-30 22:47:06 +0000

    💘😂 Checkpoint ./src/upload.js:68197850/9865 ./server.js:68197850/1828

--- src/upload.js
+++ src/upload.js
@@ -1,37 +1,27 @@
 const express = require("express");
 const Client = require("ssh2-sftp-client");
 const sftp = new Client();
-const router = express.Router(); // Crea un router Express en lugar de un servidor Express
+const router = express.Router();
 
-router.use(express.json()); // Para poder parsear el cuerpo de las solicitudes HTTP en formato JSON
+router.use(express.json());
 
 router.post("/upload", async (req, res) => {
-  const { ip: host, port, username, password } = req.body; // Obtiene la IP, el puerto, el nombre de usuario y la contraseña desde el cuerpo de la solicitud HTTP
-
-  console.log(`Host: ${host}, Port: ${port}, Username: ${username}, Password: ${password}`); // Imprime los datos recibidos en la consola
-
   const config = {
-    host,
-    port,
-    username,
-    password,
+    host: "186.1.242.50",
+    port: 8889,
+    username: "nortech",
+    password: "Nor3164!",
   };
-
-  const remotePathToList = "/";
-  const localFilePath = "./file.txt";
 
   try {
     await sftp.connect(config);
-    await sftp.put(localFilePath, remotePathToList);
-    let list = await sftp.list(remotePathToList);
-    console.log(list);
+    console.log("Connected successfully");
     await sftp.end();
-
-    res.status(200).send("File uploaded successfully."); // Envía una respuesta de éxito al frontend
+    res.status(200).send("Connected successfully");
   } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Failed to upload file."); // Envía una respuesta de error al frontend
+    console.error("Failed to connect:", error.message);
+    res.status(500).send("Failed to connect");
   }
 });
 
-module.exports = router; // Exporta el router en lugar de iniciar un servidor
+module.exports = router;
