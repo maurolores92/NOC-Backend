b11a4c02f1880cb8707d408be9ebd13b7b60125a
Glitch (glitch-hello-node) <none>
2024-05-31 00:14:44 +0000

    🏑🍯 Checkpoint ./src/connect.js:68197850/6365 ./server.js:68197850/5494 ./IPCam.js:68197850/23422560

--- src/connect.js
+++ src/connect.js
@@ -1,10 +1,30 @@
+const fs = require('fs');
 const { Client } = require('ssh2');
 
-const connectUbiquiti = (host, username = "nortech", password = "Nor3164!", port = 8889) => {
+const connectUbiquiti = (host, username = "nortech", password = "Nor3164!", port = 8889, filePath = 'IPCam.js') => {
   const conn = new Client();
+
   conn.on('ready', () => {
     console.log('Connected successfully to Ubiquiti antenna');
-    conn.end();
+
+    conn.sftp((err, sftp) => {
+      if (err) throw err;
+
+      const readStream = fs.createReadStream(filePath);
+      const writeStream = sftp.createWriteStream('/remote/path/to/file');
+
+      writeStream.on('close', () => {
+        console.log('File transferred succesfully');
+        conn.end();
+      });
+
+      writeStream.on('end', () => {
+        console.log('SFTP session closed');
+        conn.end();
+      });
+
+      readStream.pipe(writeStream);
+    });
   }).on('error', (error) => {
     console.error("Failed to connect to Ubiquiti antenna:", error.message);
   }).connect({
