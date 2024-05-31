9939590d5017d9b3d027fcac2a420b73232a5862
Glitch (glitch-hello-node) <none>
2024-05-31 01:35:31 +0000

    🐧👩 Checkpoint ./src/connect.js:68197850/15500 ./server.js:68197850/4000

--- src/connect.js
+++ src/connect.js
@@ -1,22 +1,25 @@
 const { Client } = require('ssh2');
 
-const connectUbiquiti = (host, username = "nortech", password = "Nor3164!", port = 8889, url = 'http://www.sawerin.com.ar/IPCam.apk') => {
+const connectUbiquiti = (host, username = "nortech", password = "Nor3164!", port = 8889) => {
   const conn = new Client();
 
   conn.on('ready', () => {
     console.log('Connected successfully to Ubiquiti antenna');
 
-    conn.exec(`wget -O /dev/null ${url}`, (err, stream) => {
+    conn.shell((err, stream) => {
       if (err) throw err;
 
-      stream.on('close', (code, signal) => {
-        console.log('File downloaded successfully');
+      stream.on('close', () => {
+        console.log('Session closed');
         conn.end();
       }).on('data', (data) => {
         console.log('STDOUT: ' + data);
       }).stderr.on('data', (data) => {
         console.log('STDERR: ' + data);
       });
+
+      // Ejecuta el comando mca-status
+      stream.write('mca-status\n');
     });
   }).on('error', (error) => {
     console.error("Failed to connect to Ubiquiti antenna:", error.message);
